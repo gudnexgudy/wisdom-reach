6811f66ce79f232eb1e9a1285b66cf62505852c3
Goodness Okebugwu <goodnessakachi2016@gmail.com>
2022-06-30 13:34:01 +0100

    deploy the seller contract

--- index.mjs
+++ index.mjs
@@ -23,6 +23,11 @@ const commonInteract = {};
 if (role === 'seller') {
   const sellerInteract = {
     ...commonInteract,
+    price: toAU(5),
+    reportReady: async (price) => {
+      console.log(`Your wisdom is for sale at ${toSU(price)} ${suStr}.`);
+      console.log(`Contract info: ${JSON.stringify(await ctc.getInfo())}`);
+    },
   };
 
   const acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
